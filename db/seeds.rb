a1c17ebf56c2d9eadb7470f082e150baa789c774
Alex Vassiliou <alex@20diamond.com>
2019-02-20 11:47:31 +1100

    validations complete, seed added to DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name: 'Shanghai Village',
+    address: 'Chinatown',
+    phone_number: '13 11 28',
+    category: 'chinese'
+  },
+  {
+    name: 'Universal Pizza',
+    address: 'Lygon Street',
+    phone_number: '13 44 28',
+    category: 'italian'
+  },
+  {
+    name: 'Nobu',
+    address: 'Crown Casino',
+    phone_number: '13 66 28',
+    category: 'japanese'
+  },
+  {
+    name: 'Phillipe',
+    address: 'Melbourne CBD',
+    phone_number: '13 99 28',
+    category: 'french'
+  },
+  {
+    name: 'Koko Black',
+    address: 'Carlton',
+    phone_number: '13 22 28',
+    category: 'belgian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
